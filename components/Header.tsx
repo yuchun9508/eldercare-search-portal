04d3ddcdab8421ac2b272884fbf2bfb5b2074ce5
Chun <yuchun9508@gmail.com>
2026-03-04 18:12:31 +0800

    add `Logo` component to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
+import Logo from './Logo';
 
 export default function Header() {
   return (
     <div className="bg-primary">
       <div className="container">
         <div className="flex items-center justify-between h-16">
-          {/* TODO: add logo */}
-          <Link href="/" className="text-lg font-bold text-white">
-            銀髮照護資源搜尋
+          <Link
+            href="/"
+            className="flex items-center gap-3"
+            aria-label="Eldercare home"
+          >
+            <Logo className="w-8 h-8 text-white" />
+            <span className="text-lg font-bold text-white">
+              銀髮照護資源搜尋
+            </span>
           </Link>
           <nav className="space-x-4 text-sm text-white">
             <Link href="/search">搜尋</Link>
