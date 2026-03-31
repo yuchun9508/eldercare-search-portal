62311baf203ec811d23dfecc6403205836189c29
Chun <yuchun9508@gmail.com>
2026-03-31 18:48:10 +0800

    update theme config with new palette

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="font-noto-sans-tc bg-sky-50">
+    <html lang="en" className="font-noto-sans-tc bg-neutral-50">
       <body>
         <Header />
         <main>{children}</main>
